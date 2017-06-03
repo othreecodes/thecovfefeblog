2845dcbd5b1c951010af8f40e7989e5c9f1de53c
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-03 01:21:00 +0100

    initial project structure: created Post and Category models

---
+++ blog/models.py
@@ -0,0 +1,63 @@
+# -*- coding: UTF-8 -*-
+from datetime import datetime
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(_('title'), max_length=200)
+    slug = models.SlugField(_('slug'))
+
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+        ordering = ('title',)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return ('category', None, {
+            'category': self.slug
+        })
+    get_absolute_url = models.permalink(get_absolute_url)
+
+
+class Post(models.Model):
+    """Post model."""
+    STATUS_CHOICES = (
+        (1, _('Draft')),
+        (2, _('Public')),
+    )
+    title = models.CharField(_('title'), max_length=200)
+    slug = models.SlugField(_('slug'))
+    author = models.ForeignKey(User, related_name="added_posts")
+    body = models.TextField(_('body'))
+    status = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=1)
+    publish = models.DateTimeField(_('publish'), default=datetime.now)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+    category = models.ForeignKey(
+        Category, related_name="posts",
+        blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    image = models.URLField(null=True)
+
+    class Meta:
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
+        ordering = ('-publish',)
+        get_latest_by = 'publish'
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return ('post', None, {
+            'year': self.publish.year,
+            'month': "%02d" % self.publish.month,
+            'slug': self.slug
+        })
+    get_absolute_url = models.permalink(get_absolute_url)
---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from . import views
+
+
+
+urlpatterns = [
+    # url(r'^$', views.index, name='index'),
+    # url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$', views.index, name='month_archive'),
+    # url(r'^category/(?P<category>[-\w]+)/$', views.index, name='category'),
+    # url(r'^post/(?P<year>\d{4})/(?P<month>\d{2})/(?P<slug>[-\w]+)/$',
+    #     views.post, name='post'),
+
+]
